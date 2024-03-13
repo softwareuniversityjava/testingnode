c160dd9b29c3982d525600a2f3c549ea48047bd4
softwareuniversityjava <programmingisgreatbul@gmail.com>
2024-03-13 20:02:46 +0200

    Change

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const app = express();
-const randomNumber = Math.floor(Math.random() * (10 - 1) + 1)
-app.get('/', (req, res) => {
-  res.send("The random number generated is: " + randomNumber);
-});
-
+while (true) {
+    const randomNumber = Math.floor(Math.random() * (10 - 1) + 1)
+    app.get('/', (req, res) => {
+        res.send("The random number generated is: " + randomNumber);
+    });
+}
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
